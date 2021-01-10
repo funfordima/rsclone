2d493fb2d62480f0f43fbe13c1ebec1c8d9af475
Andrew <99andreyshamrey99@gmail.com>
2021-01-10 20:17:50 +0300

    feat: main services component ready to push

---
+++ src/components/mainServices/servicesTitle/servicesTitle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './servicesTitle.css';
+
+export interface servicesTitleProps {
+  title: string
+}
+
+export default function ServicesTitle({
+    title,
+}: servicesTitleProps) {
+  return (
+    <div className='service-name'>
+      <h4>{title}</h4>
+    </div>
+  )
+}
