8aa1f6d7d17bd9177e4b1c260999b66074d1dcc0
AnastasiaFedotova <anastaisafedotova@gmail.com>
2021-01-14 21:54:24 +0300

    feat: add data types

---
+++ server/src/types/js.js
@@ -0,0 +1,116 @@
+collection.insertMany([{
+    section: "Медицинские центры и услуги",
+    subsection: "Медицинские центры",
+    title: "ЛОДЭ",
+    country: "Беларусь",
+    city: "Минск",
+    address: "пр-т Независимости, 58",
+    workingHours: "9.00 - 21.00",
+    tel: "+37529-111-11-11",
+    description: null,
+    picters: ["https://ms1.103.by/images/80c4e9b074738e185f00852f5de2b115/resize/w=250,h=82,q=94/place_logo/ba/46/d9/ba46d96470e57379cb594d0a855a1c65.png", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/94/dc/09/94dc098d8feba1d550ecc6d33b2c955d.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/7a/5e/8a/7a5e8ade84f3a675b07d86246c3cab5b.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/5e/e2/fb/5ee2fb409ba2c914add4945436438a19.jpg"],
+    complete: true
+  },
+  {
+    section: "Медицинские центры и услуги",
+    subsection: "Центры пластической хирургии",
+    title: "Центр пластической хирургии «Эдаран-Медикал»",
+    country: "Беларусь",
+    city: "Минск",
+    address: "ул. Ленина, 9",
+    workingHours: "8.00 - 20.00",
+    tel: "+37529-222-12-12",
+    description: "С центром пластической хирургии «Эдаран-Медикал» вы сможете добиться любых поставленных целей и осуществить все ваши желания, связанные с внешностью и красотой. Специалисты, работающие у нас, с удовольствием помогут вам выбрать подходящие для вас процедуры, подобрать нужного врача, а также получить всю необходимую информацию и консультацию.",
+    picters: ["https://ms1.103.by/images/80c4e9b074738e185f00852f5de2b115/resize/w=250,h=82,q=94/place_logo/dd/9b/4b/dd9b4bedeca37704e1e816cf2b97f183.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/da/8e/5a/da8e5a5907c4691bcc83753d460890e7.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/49/8e/71/498e71eaa4ace150d8d75548a45df166.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/24/e4/5a/24e45a2eee8b9c5e5251f32ab77938d0.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w%3D1200%2Ch%3D800%2Cq%3D80%2Cwatermark%3Dtrue/place_gallery_photo/a2/80/c5/a280c586c55f875f9d23aa941b6ab7e8.jpg"],
+    complete: true
+  },
+  {
+    section: "Анализы и диагностика",
+    subsection: "УЗИ",
+    title: "Медицинский центр Альфамед",
+    country: "Беларусь",
+    city: "Минск",
+    address: "пр. Независимости, 85",
+    workingHours: "8.00 - 13.00",
+    tel: "+37544212-11-12",
+    description: "Медицинский центр «Альфамед-95» — частное унитарное предприятие, открывшее свои двери для пациентов в 2001 году. Медицинский центр оказывает весь спектр медицинских услуг для жителей столицы и гостей Минска, радуя безупречным сервисом и индивидуальным подходом к каждому пациенту.",
+    picters: ["https://ms1.103.by/images/80c4e9b074738e185f00852f5de2b115/resize/w=250,h=82,q=94/place_logo/5c/e0/ff/5ce0ff21f3231670f81c0604a29be173.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/52/16/c1/5216c1478f5b8573f9e4195f4987e48e.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/ce/a9/67/cea967626907038715120e6f85df1519.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/1d/c6/8d/1dc68ddac445a7a47b4bff3c0aa8fbed.jpg"],
+    complete: true
+  },
+  {
+    section: "Вызов врача на дом",
+    subsection: "Вызов медсестры на дом",
+    title: "Медицинский центр 'Горизонт'",
+    country: "Беларусь",
+    city: "Минск",
+    address: "ул. Киселева, 12",
+    workingHours: "8.00 - 20.00",
+    tel: "+37533 712-31-42",
+    description: "В 1986 году медицинский центр «Горизонт» начал свою работу на базе медсанчасти завода «Горизонт» для обслуживания работников предприятия. Сегодня получить медицинское обслуживание в центре могут не только работники холдинга, но и все желающие — минчане и гости столицы.",
+    picters: ["https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/66/47/c2/6647c26c611a2ba6c5f58fcbb0362b06.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/93/e0/09/93e0091b894016090618b2dcf3d42130.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/63/a1/39/63a1395c270bb9c5399254ec4e46f679.jpg", "https://ms1.103.by/images/f831173f783a28436777112aa7ab6e63/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/9f/8f/b6/9f8fb6ed7dce5a4277e91b7fe8654321.jpg"],
+    complete: true
+  },
+  {
+    section: "Медицинские центры",
+    subsection: "Андрология",
+    title: "Клиника урологии доктора Сударикова И.В. «Консилиум»",
+    country: "Украина",
+    city: "Киев",
+    address: "ул. Раисы Окипной, 4а",
+    workingHours: "6.30 - 13.00",
+    tel: "380 67 778-14-22",
+    description: "Мы открылись! Приходите и убедитесь в том, что у нас работают лучше врачи.",
+    picters: ["https://ms1.103.ua/images/d028e8a70fe894373baa527708b1bdf6/resize/w=250,h=82,q=94/place_logo/0e/97/2b/0e972b3116245ae5311a93b3206f65e6.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/fd/90/5b/fd905bdfe607478ed6f8b63817c147ed.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/15/96/7e/15967e744c8541f0bcaaa35f9fb4f6a0.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/da/f6/ed/daf6ed8fa6a2aac358c4d78aaaf0eebc.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/8d/f3/70/8df370f50fc3bf79f50dd4285f495bfe.jpg"],
+    complete: true
+  },
+  {
+    section: "Психологическая помощь",
+    subsection: "Лечение зависимостей",
+    title: "Медицинский центр лечения алкоголизма «Адомед»",
+    country: "Украина",
+    city: "Киев",
+    address: "ул. Леся Курбаса, 1Б",
+    workingHours: "6.30 - 13.00",
+    tel: "380 95 125-33-55",
+    description: null,
+    picters: ["https://ms1.103.ua/images/d028e8a70fe894373baa527708b1bdf6/resize/w=250,h=82,q=94/place_logo/fd/66/6e/fd666ec7d614abc46d8f7ea172bc820b.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/6b/d5/25/6bd5257edc39655e37c065709ad537a0.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/4b/b8/f4/4bb8f4712f1d0463050064c4d66cc419.jpg"],
+    complete: true
+  },
+  {
+    section: "Вызов врача на дом",
+    subsection: "Вызов терапевта на дом",
+    title: "Сеть медицинских центров для детей «KinderKlinik (КиндерКлиник)»",
+    country: "Украина",
+    city: "Киев",
+    address: "ул. Демеевская, 16",
+    workingHours: "8.30 - 20.30",
+    tel: "380 96 390-11-22",
+    description: `«KinderKlinik» проводит комплексные осмотры детей на дому.
+    Новорожденным малышам – мы проведем патронаж.
+    Детям, которые собираются идти в садик/ школу — мы проведем комплексный осмотр и выдадим все необходимые медицинские справки.
+    Детям, у которых появилась проблема со здоровьем и требуется осмотр сразу нескольких специалистов.
+    Всем детям — мы проведем плановый осмотр с профилактической целью.
+    Наши врачи приедут к Вам домой и проведут осмотр Вашего малыша с соблюдением всех мер предосторожности и дезинфекции: доктора наденут маски, одноразовые халаты и бахилы, продезинфицируют перчатки антисептиком и примут другие необходимые меры защиты.
+    Вы получите консультации детских врачей, не выходя из дома — отсутствует риск подхватить коронавирус или другую опасную инфекцию.
+    Быстро и удобно.
+    Вы получите комплексную консультацию, а в результате ее — полную картину о состоянии здоровья и развития Вашего ребенка.
+    *Пакеты не распространяются на консультации врачей–экспертов, ведущих специалистов и главврача.
+    Обращайтесь! И будьте здоровы!"`,
+    picters: ["https://ms1.103.ua/images/d028e8a70fe894373baa527708b1bdf6/resize/w=250,h=82,q=94/place_logo/2f/c6/3a/2fc63a9f6b2ab9d83e2f4e1af0b56b01.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/38/84/b9/3884b98e39280fef2a1b8883e7e1bd7f.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/8e/a2/18/8ea218450d088f23151d1e617dc0ce6b.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/48/87/a0/4887a07595ee995f909f3dac74299204.jpg", "https://ms1.103.ua/images/fabb9523862ba9745a8b255b01d9d1fd/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/70/54/26/7054269563c3f5760ebd8858d6aa6fb3.jpg"],
+    complete: true
+  },
+  {
+    section: "Экстренная медицина",
+    subsection: "Центры переливания крови",
+    title: "Дорожная станция переливания крови",
+    country: "Украина",
+    city: "Киев",
+    address: "ул. Николая Пимоненко, 8",
+    workingHours: "8.30 - 20.30",
+    tel: "380 44 465-20-65",
+    description: `Метро: Лукьяновская, Район: Правый берег, Центр, Шевченковский, Микрорайон: Кудрявец`,
+    picters: null,
+    complete: true
+  }
+
+])
