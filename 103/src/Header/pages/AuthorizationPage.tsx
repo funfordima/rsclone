12d3ee02bd535d37a459919e51150a755ec516fb
funfordima <d.g.litvinov@gmail.com>
2021-01-19 20:27:53 +0200

    feat: initializa firebase && add StyledFirebaseAuth component to registration from social network

--- 103/src/Header/pages/AuthorizationPage.tsx
+++ 103/src/Header/pages/AuthorizationPage.tsx
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
-import { NavLink, BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Fragment, useEffect, useState } from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
 import './AuthorizationPage.scss';
 import styled from 'styled-components';
-import { RegistrationForm } from './RegistrationForm';
-import { RegistrationPage } from '../pages/RegistrationPage';
-import { MainPage } from '../pages/MainPage';
+import { LogInPage } from './LogInPage';
+import { SignInPage } from './SignInPage';
+import { SocialBefore, AlertError, BackButton } from '../styledComponents';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -18,37 +22,6 @@ const Content = styled.div`
 
 const Header = styled.div`
 
-`;
-
-const BackButton = styled.a`
-  color: #fff;
-  padding: 16px 16px 16px 0;
-  display: inline-flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  font-size: 15px;
-  line-height: 24px;
-  transition-property: background-color, color;
-  transition-duration: 0.2s;
-  transition-timing-function: ease-in-out;
-  cursor: pointer;
-
-  & span {
-    margin-left: 5px;
-  }
-
-  & svg {
-    fill: #000;
-    vertical-align: bottom;
-    transform-origin: center;
-    transform: rotate(180deg);
-    border-radius: 50%;
-    background-color: #ccc;
-  }
-
-  &:hover {
-    color: rgba(255,255,255,0.8);
-  }
 `;
 
 const ContentHeader = styled.div`
@@ -108,9 +81,39 @@ const ContentWrapper = styled.div`
   }
 `;
 
-export const AuthorizationPage: React.FC = () => {
+const config = {
+  apiKey: "AIzaSyCaU0-FbnHAag9PF8IbRrHAfNnrP9G-xXw",
+  authDomain: "react-app-rsclone.firebaseapp.com",
+  databaseURL: "https://react-app-rsclone-default-rtdb.firebaseio.com",
+  projectId: "react-app-rsclone",
+  storageBucket: "react-app-rsclone.appspot.com",
+  messagingSenderId: "488595044140",
+  appId: "1:488595044140:web:88056b9ed8c5a660dc5e89"
+};
+
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  },
+};
+
+interface AuthorizationPageProps {
+  onToggleEnterUser: () => void;
+}
+
+export const AuthorizationPage: React.FC<AuthorizationPageProps> = ({ onToggleEnterUser }) => {
   const addBodyClass = (className: string): void => document.body.classList.add(className);
   const removeBodyClass = (className: string): void => document.body.classList.remove(className);
+  const [isErrorSignIn, setErrorSignIn] = useState(false);
 
   useEffect(() => {
     addBodyClass('body');
@@ -120,41 +123,48 @@ export const AuthorizationPage: React.FC = () => {
     }
   }, ['body']);
 
+  const toggleErrorComponent = (isError: boolean): void => {
+    setErrorSignIn(isError);
+  }
+
   return (
     <Fragment>
-      <BrowserRouter>
-        <Wrapper>
-          <Content>
-            <Header>
-              <BackButton href="/">
-                <svg
-                  width="24"
-                  height="24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.3 7.5a.4.4 0 010-.7c.3-.2.6-.1.8 0l4.6 4.9v.6L10 17.1c-.2.2-.5.3-.7 0a.4.4 0 010-.6l4.2-4.5-4.3-4.5z" />
-                </svg>
-                <span>103.ua</span>
-              </BackButton>
-            </Header>
-            <ContentHeader>
-              <div>
-                <NavLink className="tabsItem" to="/authorization">LogIn</NavLink>
-                <NavLink className="tabsItem" to="/registration">SignIn</NavLink>
-              </div>
-            </ContentHeader>
-            <ContentWrapper>
-              <Switch>
-                <Route exact path="/authorization" component={RegistrationForm} />
-                <Route path="/registration" component={RegistrationPage} />
-                {/* <Route path="/restore" component={MainPage} /> */}
-              </Switch>
-              {/* <RegistrationForm /> */}
-              {/* <NavLink to="/restore">Forgot password?</NavLink> */}
-            </ContentWrapper>
-          </Content>
-        </Wrapper>
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      <Wrapper>
+        <Content>
+          <Header>
+            <BackButton to="/">
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.3 7.5a.4.4 0 010-.7c.3-.2.6-.1.8 0l4.6 4.9v.6L10 17.1c-.2.2-.5.3-.7 0a.4.4 0 010-.6l4.2-4.5-4.3-4.5z" />
+              </svg>
+              <span>103.ua</span>
+            </BackButton>
+          </Header>
+          <ContentHeader>
+            <div>
+              <NavLink exact className="tabsItem" to="/authorization">LogIn</NavLink>
+              <NavLink className="tabsItem" to="/authorization/registration">SignIn</NavLink>
+            </div>
+          </ContentHeader>
+          <ContentWrapper>
+            {isErrorSignIn
+              && (<AlertError> Invalid E-mail or password!</AlertError>)
+            }
+            <Switch>
+              <Route exact path="/authorization" render={() => <LogInPage onToggleErrorComponent={toggleErrorComponent} toggleEnterUser={onToggleEnterUser} />} />
+              <Route path="/authorization/registration" component={SignInPage} />
+              {/* <Route path="/restore" component={MainPage} /> */}
+            </Switch>
+            <SocialBefore>sign in via social networks</SocialBefore>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+          </ContentWrapper>
+        </Content>
+      </Wrapper>
+      {/* </BrowserRouter> */}
     </Fragment >
   );
 };
