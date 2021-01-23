45f1925d18c4b4ef8c219b955f4577bb99b44c2b
funfordima <d.g.litvinov@gmail.com>
2021-01-23 20:01:59 +0200

    feat: add load/change user avatar photo

--- 103/src/Header/pages/components/UserContentPage.tsx
+++ 103/src/Header/pages/components/UserContentPage.tsx
@@ -4,6 +4,7 @@ import { ContentTitle, UserForm, FormField, ButtonSave, AlertError, AlertSuccess
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const UserPhotoWrapper = styled.div`
   display: flex;
@@ -40,63 +41,87 @@ const ButtonCancel = styled(ButtonSave)`
 
 export const UserContentPage: React.FC = () => {
   const user = firebase.auth().currentUser;
+  const storageRef = firebase.storage().ref().child(user?.email + '/profile.jpg');
   let name = '';
   let surname = '';
   if (user?.displayName !== null) {
     [name, surname] = user?.displayName.split(' ');
   }
   const mail = user?.email || '';
-  const photo = user?.photoURL || '';
-  const [firstName, setFirstName] = useState(name);
-  const [lastName, setLastName] = useState(surname);
-  const [email, setEmail] = useState(mail);
-  const [isPhoto, setPhoto] = useState(photo);
+  // const photo = user?.photoURL || '';
+  // console.log(photo);
+  console.log(user);
+  // const [firstName, setFirstName] = useState(name);
+  // const [lastName, setLastName] = useState(surname);
+  // const [email, setEmail] = useState(mail);
+
+  const [stateUser, setStateUser] = useState({
+    'firstName': name,
+    'lastName': surname,
+    'email': mail
+  });
+
+  const [isPhoto, setPhoto] = useState(user?.photoURL);
   const [isError, setError] = useState('');
   const [isSuccess, setSuccess] = useState('');
 
+  const downloadData = (): void => {
+    storageRef.getDownloadURL()
+      .then((url) => {
+        setPhoto(url);
+      })
+      .catch((error) => {
+        console.error(`Something went wrong, ${error.message}`);
+        setPhoto(user?.photoURL);
+      });
+  };
+
+  downloadData();
+
   const changeName = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setFirstName(event.target.value.trim());
+    setStateUser(() => ({ ...stateUser, 'firstName': event.target.value.trim() }));
+    // setFirstName(event.target.value.trim());
     setError('');
   };
 
   const changeLastName = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setLastName(event.target.value.trim());
+    setStateUser(() => ({ ...stateUser, ...{ 'lastName': event.target.value.trim() } }));
+    // setLastName(event.target.value.trim());
     setError('');
   };
 
   const changeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setEmail(event.target.value.trim());
+    setStateUser(() => ({ ...stateUser, ...{ 'email': event.target.value.trim() } }));
+    // setEmail(event.target.value.trim());
     setError('');
-  };
-
-  const changePhoto = (): void => {
-    console.log('load photo');
   };
 
   const handleBtnSaveClick = (): void => {
     const minNameLength = 3;
-    if (!firstName || firstName.trim().length < minNameLength || firstName[0].match(/\d/)) {
+    if (!stateUser.firstName || stateUser.firstName.trim().length < minNameLength || stateUser.firstName[0].match(/\d/)) {
       setError('Name should be at least 4 symbols, don\'t starts with decimal!');
     }
-    if (!lastName || lastName.trim().length < minNameLength || lastName[0].match(/\d/)) {
+    if (!stateUser.lastName || stateUser.lastName.trim().length < minNameLength || stateUser.lastName[0].match(/\d/)) {
       setError('LastName should be at least 4 symbols, don\'t starts with decimal!');
     }
-    if (!email || email.trim().length < minNameLength || !email.includes('@') || !email.split('@')[1].includes('.')) {
+    if (!stateUser.email || stateUser.email.trim().length < minNameLength || !stateUser.email.includes('@') || !stateUser.email.split('@')[1].includes('.')) {
       setError('Email should be at least 4 symbols, must contain @, and contain ends with "."');
     }
 
     user.updateProfile({
-      displayName: `${firstName} ${lastName}`,
+      displayName: `${stateUser.firstName} ${stateUser.lastName}`,
       photoURL: `${isPhoto}`,
     }).then(() => {
-      setFirstName(firstName.toString());
-      setLastName(lastName.toString());
+      // setFirstName(firstName.toString());
+      // setLastName(lastName.toString());
+      // set Image Name
     }).catch((error) => {
       setError(error.message);
     });
 
-    user.updateEmail(`${email}`).then(() => {
-      setEmail(email);
+    user.updateEmail(`${stateUser.email}`).then(() => {
+      // setEmail(email);
+      //set Email
     }).catch((error) => {
       setError(error.message);
     });
@@ -107,17 +132,53 @@ export const UserContentPage: React.FC = () => {
   };
 
   const handleBtnCancelClick = (): void => {
-    setFirstName(name);
-    setLastName(name);
-    setEmail(mail);
+    setStateUser({
+      'firstName': name,
+      'lastName': surname,
+      'email': mail
+    });
+    // setFirstName(name);
+    // setLastName(name);
+    // setEmail(mail);
   };
+
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const selectedFile = event.target!.files[0];
+    const reader = new FileReader();
+
+    if (!selectedFile.type.match('image.*')) {
+      setError("Image only please....");
+    }
+
+    reader.onload = function (e) {
+      const b64 = e.target.result.split(',')[1];
+      const byteCharacters = atob(decodeURIComponent(b64));
+      const byteNumbers = new Array(byteCharacters.length);
+
+      for (let i = 0; i < byteCharacters.length; i += 1) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      const blob = new Blob([byteArray], { type: 'text/plain' });
+      // const imageUrl = URL.createObjectURL(blob, { type: 'text/plain' });
+
+      // Create a Storage Ref w/ username
+      const storageRef = firebase.storage().ref().child(user?.email + '/profile.jpg');
+
+      // Upload file
+      const task = storageRef.put(byteArray).then(() => setPhoto(reader.result));
+    };
+    reader.readAsDataURL(selectedFile);
+  }
 
   return (
     <>
       <UserPhotoWrapper>
         <UserPhoto
           src={isPhoto}
-          alt={`${firstName}'s photo`}
+          alt={`${stateUser.firstName}'s photo`}
         />
       </UserPhotoWrapper>
       <ContentTitle>
@@ -130,7 +191,7 @@ export const UserContentPage: React.FC = () => {
             id='firstName'
             placeholder='Name'
             name='firstName'
-            value={firstName}
+            value={stateUser.firstName}
             onChange={changeName}
           />
         </FormField>
@@ -141,7 +202,7 @@ export const UserContentPage: React.FC = () => {
             id='lastName'
             placeholder='LastName'
             name='lastName'
-            value={lastName}
+            value={stateUser.lastName}
             onChange={changeLastName}
           />
         </FormField>
@@ -151,7 +212,7 @@ export const UserContentPage: React.FC = () => {
             id='user-email'
             placeholder='E-mail'
             name='email'
-            value={email}
+            value={stateUser.email}
             onChange={changeEmail}
           />
         </FormField>
@@ -163,7 +224,7 @@ export const UserContentPage: React.FC = () => {
             placeholder='Add your photo'
             name='userPhoto'
             accept='image/*'
-            onChange={changePhoto}
+            onChange={handleFileSelect}
           />
         </FormField>
         <FormFields>
