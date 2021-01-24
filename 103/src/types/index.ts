0cfe95f40d23cdf17c37c8fe752c79c3322d9d94
AnastasiaFedotova <anastaisafedotova@gmail.com>
2021-01-24 01:00:03 +0100

    feat: add component types

---
+++ 103/src/types/index.ts
@@ -0,0 +1,42 @@
+export type ClinicType = {
+    _id: string,
+    section: string,
+    subsection: string,
+    title: string,
+    country: string,
+    city: string,
+    address: string,
+    workingHours: string | null,
+    tel: string | null,
+    description: string | null,
+    pictures: Array<string> | null,
+    personnelID: Array<string> | null,
+    coordinates: Array<number>,
+    complete: boolean
+}
+
+export type DoctorType = {
+    _id: string,
+    name: string,
+    country: string,
+    city: string,
+    section: string,
+    profession: string,
+    experience: string | null,
+    category: string | null,
+    education: string | null,
+    placeWork: string | null,
+    idWork: string | null,
+    tel: string | null,
+    pictures: Array<string> | null,
+    complete: boolean
+}
+
+export type ArticleType = {
+    _id: string,
+    title: string,
+    subtitle: string | null,
+    text: string,
+    src: string | null,
+    complete: boolean
+}
