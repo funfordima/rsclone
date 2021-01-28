a221563a8d1a1408b4f6ff234f1efce3e3e10833
funfordima <d.g.litvinov@gmail.com>
2021-01-28 20:21:42 +0200

    fix: change function toggleVisiblePassword in components SignInPage && LogInPage

--- 103/src/components/Header/pages/SignInPage.tsx
+++ 103/src/components/Header/pages/SignInPage.tsx
@@ -6,91 +6,97 @@ import 'firebase/auth';
 
 export const SignInPage: React.FC = () => {
   const [isError, setError] = useState('');
-  // const [isSuccess, setSuccess] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [isOpenPassword, setOpenPassword] = useState(false);
+  const [isOpenConfirmPassword, setOpenConfirmPassword] = useState(false);
+  const [userData, setUserData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    newPassword: '',
+    confirmedPassword: '',
+  });
 
-  const toggleVisiblePassword = (event: React.MouseEvent): void => {
-    const span = event.target;
-    span.previousElementSibling.setAttribute('type', span.previousElementSibling.getAttribute('type') === 'text' ? 'password' : 'text');
+  const toggleVisibleConfirmPassword = (): void => {
+    setOpenConfirmPassword((isOpenConfirmPassword) => !isOpenConfirmPassword);
+  };
+
+  const toggleVisiblePassword = (): void => {
+    setOpenPassword((isOpenPassword) => !isOpenPassword);
   };
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setFirstName(event.target.value.trim());
+    setUserData(() => ({ ...userData, 'firstName': event.target.value.trim() }));
     setError('');
   };
 
   const changeLastName = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setLastName(event.target.value.trim());
+    setUserData(() => ({ ...userData, 'lastName': event.target.value.trim() }));
     setError('');
   };
 
   const changeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setEmail(event.target.value.trim());
+    setUserData(() => ({ ...userData, 'email': event.target.value.trim() }));
     setError('');
   };
 
   const handleSetNewPassword = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setNewPassword(event.target.value.trim());
+    setUserData(() => ({ ...userData, 'newPassword': event.target.value.trim() }));
     setError('');
   };
 
   const handleConfirmPassword = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setConfirmedPassword(event.target.value.trim());
+    setUserData(() => ({ ...userData, 'confirmedPassword': event.target.value.trim() }));
     setError('');
   };
 
   const signInAccount = (): void => {
+    const { firstName, lastName, email, newPassword, confirmedPassword } = userData;
     const minNameLength = 3;
     const minLengthPassword = 8;
     const regExp = new RegExp(`^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([^_])([a-zA-Z0-9]){${minLengthPassword},}$`);
 
-    if (!firstName || firstName.trim().length < minNameLength || firstName[0].match(/\d/)) {
-      setError('Name should be at least 3 symbols, don\'t starts with decimal!');
+    switch (true) {
+      case (!firstName || firstName.trim().length < minNameLength || firstName[0].match(/\d/)): {
+        setError('Name should be at least 4 symbols, don\'t starts with decimal!');
+        break;
+      }
+      case (!lastName || lastName.trim().length < minNameLength || lastName[0].match(/\d/)): {
+        setError('LastName should be at least 4 symbols, don\'t starts with decimal!');
+        break;
+      }
+      case (!email || email.trim().length < minNameLength || !email.includes('@') || !email.split('@')[1].includes('.')): {
+        setError('Email should be at least 4 symbols, must contain @, and contain ends with "."');
+        break;
+      }
+      case (!newPassword.match(regExp)): {
+        setError('Password must contains a lowercase letters, an uppercase letters, number and be at least 8 characters.');
+        break;
+      }
+      case (confirmedPassword !== newPassword): {
+        setError('Passwords do not match');
+        break;
+      }
+      default: {
+        firebase.auth().createUserWithEmailAndPassword(email, newPassword)
+          .then(() => {
+            const user = firebase.auth().currentUser;
+
+            user.updateProfile({
+              displayName: `${firstName} ${lastName}`,
+            }).catch((error) => {
+              setError(error.message);
+            });
+
+            user.updateEmail(`${email}`).catch((error) => {
+              setError(error.message);
+            });
+
+          })
+          .catch((error) => {
+            setError(error.message);
+          });
+      }
     }
-    if (!lastName || lastName.trim().length < minNameLength || lastName[0].match(/\d/)) {
-      setError('LastName should be at least 4 symbols, don\'t starts with decimal!');
-    }
-    if (!email || email.trim().length < minNameLength || !email.includes('@') || !email.split('@')[1].includes('.')) {
-      setError('Email should be at least 4 symbols, must contain @, and contain ends with "."');
-    }
-
-    if (!newPassword.match(regExp)) {
-      setError('Password must contains a lowercase letters, an uppercase letters, number and be at least 8 characters.');
-    }
-
-    if (confirmedPassword !== newPassword) {
-      setError('Passwords do not match');
-    }
-
-    firebase.auth().createUserWithEmailAndPassword(email, newPassword)
-      .then(() => {
-        const user = firebase.auth().currentUser;
-        // setSuccess('Successful login');
-
-        user.updateProfile({
-          displayName: `${firstName} ${lastName}`,
-        }).then(() => {
-          // setFirstName(firstName.toString());
-          // setLastName(lastName.toString());
-        }).catch((error) => {
-          setError(error.message);
-        });
-
-        user.updateEmail(`${email}`).then(() => {
-          // setEmail(email);
-        }).catch((error) => {
-          setError(error.message);
-        });
-
-        // setTimeout(() => setSuccess(''), 1000);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
   }
 
   return (
@@ -117,7 +123,7 @@ export const SignInPage: React.FC = () => {
         />
         <FormField>
           <InputText
-            type="password"
+            type={isOpenPassword ? "text" : "password"}
             placeholder="Password"
             name="password"
             autocomplete="new-password"
@@ -132,12 +138,12 @@ export const SignInPage: React.FC = () => {
         {/* {isSuccess && <AlertSuccess>{isSuccess}</AlertSuccess>} */}
         <FormField>
           <InputText
-            type="password"
+            type={isOpenConfirmPassword ? "text" : "password"}
             placeholder="Password confirm"
             name="password-confirm"
             onChange={handleConfirmPassword}
           />
-          <TogglePassword onClick={toggleVisiblePassword} />
+          <TogglePassword onClick={toggleVisibleConfirmPassword} />
         </FormField>
         <InputBtnSignIn
           type="button"
