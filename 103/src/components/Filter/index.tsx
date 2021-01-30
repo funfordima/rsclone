b9ac232d059322b7e87e670605c58cdc7af12568
Andrey Znak <a.y.znak@gmail.com>
2021-01-30 17:25:29 +0300

    feat: add filter component

--- 103/src/components/Filter/index.tsx
+++ 103/src/components/Filter/index.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import './index.scss';
 
-const Filter: React.FC = () => {
-    return <></>;
+import FilterItem from './FilterItem';
+import { ISubCategory } from '../../interfaces/interfaces';
+import {
+  Subcategory,
+  SubcategoriesMenu,
+  SubcategoriesItem,
+} from '../../types/index';
+
+interface ISubCategoriesList {
+  subCategoriesList: Subcategory[];
+  currentPageId: string | null;
+}
+
+const Filter: React.FC<ISubCategoriesList> = ({
+  subCategoriesList,
+  currentPageId,
+}) => {
+  let subCategoriesMenu: ISubCategory[] = [];
+
+  subCategoriesList.forEach((subCategories: any) => {
+    subCategories.categoriesId === currentPageId
+      ? (subCategoriesMenu = subCategories.itemsMenu)
+      : null;
+  });
+
+  return (
+    <div
+      className={
+        currentPageId === null ? 'categorySubMenu' : 'categorySubMenu active'
+      }
+    >
+      <div className="categorySubMenu-row">
+        {subCategoriesMenu.map((subCategory: any, index: number) => {
+          return (
+            <FilterItem
+              key={index}
+              icon={subCategory.icon}
+              title={subCategory.title}
+              subcategoriesItem={subCategory.items}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
