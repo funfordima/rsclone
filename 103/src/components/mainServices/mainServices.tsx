44bbe97c4c14e2fb936aa086b716f20471d725d3
Andrey Znak <a.y.znak@gmail.com>
2021-02-03 11:58:44 +0300

    refactor: component like arrow function

--- 103/src/components/mainServices/mainServices.tsx
+++ 103/src/components/mainServices/mainServices.tsx
@@ -4,22 +4,24 @@ import ServicesTitle from './servicesTitle/servicesTitle';
 import ServicesContent from './servicesContent/servicesContent';
 
 export interface mainServicesProps {
-  serviceName: string
-  serviceLinks: string[]
-  serviceImagesLinks: string[]
+  serviceName: string;
+  serviceLinks: string[];
+  serviceImagesLinks: string[];
 }
 
-export default function MainServices({
+const MainServices: React.FC<mainServicesProps> = ({
   serviceName,
   serviceLinks,
   serviceImagesLinks,
-}: mainServicesProps) {
+}) => {
   return (
-    <div className='services-wrapper'>
-      <div className='services-container'>
-        <ServicesTitle title = {serviceName}/>
-        <ServicesContent links = {serviceLinks} images = {serviceImagesLinks} />
+    <div className="services-wrapper">
+      <div className="services-container">
+        <ServicesTitle title={serviceName} />
+        <ServicesContent links={serviceLinks} images={serviceImagesLinks} />
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default MainServices;
